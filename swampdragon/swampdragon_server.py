3f65f1ce797a7bf199eea4e3375306dde4ca7616
Yonk <nopctoday@gmail.com>
2015-05-04 17:21:15 -0700

    added support for heroku (running wsgi and websocket under one dyno)

--- swampdragon/swampdragon_server.py
+++ swampdragon/swampdragon_server.py
@@ -1,10 +1,13 @@
 import django
 from django.conf import settings
 from django.utils.importlib import import_module
-from tornado import web, ioloop
+from tornado import web, ioloop, wsgi
 from sockjs.tornado import SockJSRouter
 from swampdragon import discover_routes, load_field_deserializers
 from swampdragon.settings_provider import SettingsHandler
+from django.core.wsgi import get_wsgi_application
+import pdb
+import os
 
 
 def run_server(host_port=None):
@@ -41,7 +44,15 @@ Update your settings and add SWAMP_DRAGON_CONNECTION.
     urls = discover_routes()
     for router in routers:
         urls += router.urls
+    
+    wsgi_app = wsgi.WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
     urls.append(('/settings.js$', SettingsHandler))
+    
+    staticurl =  os.path.join(settings.BASE_DIR, 'staticfiles/')
+    
+    urls.append((r'/static/(.*)', web.StaticFileHandler, {'path':staticurl}))
+    urls.append((r".*", web.FallbackHandler, dict(fallback=wsgi_app)))
+    
 
     load_field_deserializers()
 
